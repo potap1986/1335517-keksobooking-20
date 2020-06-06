fde0bf4277156ae8b7dc9cd9687005c7e531001a
potap1986 <potap1986@inbox.ru>
2020-06-06 13:43:55 +0300

    Личный проект: больше деталей (часть 1) ИСПРАВИЛ

--- js/main.js
+++ js/main.js
@@ -11,41 +11,64 @@ var announcements = [];
 var announcementsQantity = 8;
 var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var roomsMIN = 1;
+var roomsMAX = 3;
+var guestsMIN = 0;
+var guestsMAX = 2;
+
+var getAvatar = function (index) {
+  return 'img/avatars/user0' + (index + 1) + '.png';
+};
+
+var getTitle = function (index) {
+  return 'Заголовок предложения ' + (index + 1);
+};
+
+var getNewOfferFeatures = function () {
+  var arr = [];
+  for (var i = 0; i < getRandomNumber(0, OFFER_FEATURES.length); i++) {
+    if (getRandomNumber(0, 1)) {
+      arr.push(OFFER_FEATURES[i]);
+    }
+  }
+  return arr;
+};
+
+var getDescription = function (index) {
+  return 'Описание ' + (index + 1);
+};
 
 var createAnnouncements = function (count) {
   for (var i = 0; i < count; i++) {
-    var object = {};
-    var author = {};
-    var offer = {};
-    var location = {};
-    author.avatar = 'img/avatars/user0' + getRandomNumber(1, 8) + '.png';
-    object.author = author;
-    offer.title = 'Заголовок предложения ' + (i + 1);
-    offer.address = getRandomNumber(100, 900) + ', ' + getRandomNumber(100, 900);
-    offer.price = getRandomNumber(1000, 10000);
-    offer.type = OFFER_TYPE[getRandomNumber(0, 3)];
-    offer.rooms = getRandomNumber(1, 6);
-    offer.guests = getRandomNumber(1, 10);
-    offer.checkin = '1' + getRandomNumber(2, 4) + ':00';
-    offer.checkout = '1' + getRandomNumber(2, 4) + ':00';
-    offer.features = [];
-    for (var j = 0; j < getRandomNumber(0, OFFER_FEATURES.length); j++) {
-      if (getRandomNumber(0, 1)) {
-        offer.features.push(OFFER_FEATURES[j]);
+    var locationX = getRandomNumber(50, 700);
+    var locationY = getRandomNumber(130, 630);
+    var object = {
+      author: {
+        avatar: getAvatar(i)
+      },
+      offer: {
+        title: getTitle(i),
+        address: locationX + ', ' + locationY,
+        price: getRandomNumber(1000, 100000),
+        type: OFFER_TYPE[getRandomNumber(0, 3)],
+        rooms: getRandomNumber(roomsMIN, roomsMAX),
+        guests: getRandomNumber(guestsMIN, guestsMAX),
+        checkin: '1' + getRandomNumber(2, 4) + ':00',
+        checkout: '1' + getRandomNumber(2, 4) + ':00',
+        features: getNewOfferFeatures(),
+        description: getDescription(i),
+        photos: 'http://o0.github.io/assets/images/tokyo/hotel' + getRandomNumber(1, 3) + '.jpg',
+      },
+      location: {
+        x: locationX,
+        y: locationY
       }
-    }
-    offer.description = 'Описание ' + (i + 1);
-    offer.photos = 'http://o0.github.io/assets/images/tokyo/hotel' + getRandomNumber(1, 3) + '.jpg';
-    object.offer = offer;
-    location.x = getRandomNumber(50, 700);
-    location.y = getRandomNumber(130, 630);
-    object.location = location;
+    };
     announcements.push(object);
   }
 };
 
 createAnnouncements(announcementsQantity);
-
 
 var renderPin = function (advert) {
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
